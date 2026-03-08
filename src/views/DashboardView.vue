9ed32526af963b44baa7fc97440813ffcf5d8801
Ian <orozcopaniaguaian@gmail.com>
2026-03-08 15:07:31 -0600

    Replace old phone mockup image with a new asset and update the image source in DashboardView

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -60,7 +60,7 @@ const sidebarStore = useSidebarStore()
             </div>
             <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
               <img
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
+                src=".../assets/phone-mockup.png"
                 alt="mockup"
               />
             </div>
