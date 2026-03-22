b8638aa6f6afa88510d4d4a5cf8e476ccc97daa2
Haziel <rzrzhaziel@gmail.com>
2026-03-22 17:16:22 -0600

    Some fix

---
+++ src/views/ForgotPasswordView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <div class="flex items-center justify-center min-h-screen w-full bg-white">
+      <div
+        class="p-16 bg-white text-dark rounded-lg shadow-md border border-dark w-full md:w-1/2 lg:w-1/3"
+      >
+        <h1 class="text-3xl font-bold mt-4">Recuperar Contraseña</h1>
+        <p class="text-sm font-bold mt-2">
+          Ingresa tu correo electrónico y te enviaremos un enlace para que puedas crear una nueva
+          contraseña.
+        </p>
+        <form @submit.prevent="" class="mt-8 w-full">
+          <input
+            required
+            type="email"
+            placeholder="Correo Electrónico"
+            class="w-full px-4 py-2 mt-4 border-2 border-[#C3C3C3] rounded-lg focus:outline-none hover:border-dark focus:border-primary placeholder-[#C3C3C3] transition duration-300"
+          />
+          <input
+            type="submit"
+            value="Restablecer Contraseña"
+            class="w-full px-4 py-2 mt-6 bg-primary text-white font-medium rounded-lg hover:bg-dark cursor-pointer transition duration-300"
+          />
+        </form>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ForgotPasswordView',
+}
+</script>
