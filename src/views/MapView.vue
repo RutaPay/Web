c4d60960cfaa21d811db1e3af5ad679f814bf1a4
Ian <orozcopaniaguaian@gmail.com>
2026-04-08 18:54:39 -0600

    Refactor MapView layout and enhance suggested routes section for improved user experience

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -32,38 +32,6 @@ const filteredRoutes = computed(() => {
 </script>
 
 <template>
-
-  <!-- <SideBar />
-  <main
-    class="min-h-screen transition-all duration-500 bg-gray-50 p-6 md:p-10"
-    :class="{ 'ml-64': !sidebarStore.closedState, 'ml-28': sidebarStore.closedState }"
-  >
-    <div
-      class="min-h-screen flex flex-row bg-white text-text-dark rounded-3xl shadow-sm border border-gray-200 p-4 md:p-8"
-    >
-    <div class="w-1/2 flex items-baseline flex-col p-8">
-      <input type="text" placeholder="Busca una ruta..." class="w-full p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary mb-4"
-             id="search"/>
-      <div class="w-full h-128 bg-white rounded-lg flex items-baseline justify-baseline text-black flex-col gap-10">
-        <div class="text-2xl p-5 text-primary">Rutas Sugeridas</div>
-        <div class="w-full h-96 bg-gray-200 rounded-lg p-5 overflow-y-auto">
-          <ul id="routeList">
-            <li class="hover:text-primary "><span class="text-secondary font-bold">001</span> PEDREGOSO - CENTRO</li>
-            <li class="hover:text-primary"><span class="text-secondary font-bold">002</span> PEDREGOSO - UNIVERSIDAD TECNOLÓGICA</li>
-            <li class="hover:text-primary"><span class="text-secondary font-bold">004</span> COL. INDUSTRIAL NUEVO SAN JUAN - CENTRO</li>
-            <li class="hover:text-primary"><span class="text-secondary font-bold">005</span> PEDREGOSO 2A. SECCIÓN - CENTRAL DE AUTOBUSES</li>
-            <li class="hover:text-primary"><span class="text-secondary font-bold">007</span> CENTRAL DE AUTOBUSES - UNIVERSIDAD TECNOLÓGICA</li>
-           </ul>
-        </div>
-      </div>
-    </div>
-    <div class="w-1/2 p-8">
-
-    </div>
-    </div>
-  </main>
-  <Footer :isClosed="sidebarStore.closedState" /> -->
-
   <SideBar />
   <main
     class="min-h-screen transition-all duration-500 bg-gray-50 p-6 md:p-10"
@@ -82,11 +50,11 @@ const filteredRoutes = computed(() => {
           v-model="search"
         />
 
-        <div class="w-full h-128 bg-white rounded-lg flex items-baseline justify-baseline text-black flex-col gap-10">
-          <div class="text-2xl p-5 text-primary">Rutas Sugeridas</div>
+        <div class="w-full h-128 bg-white rounded-lg flex items-baseline justify-baseline text-black flex-col gap-10 pr-5">
+          <div class="text-2xl p-5 text-primary border-b border-gray-300 hover:text-black">Rutas Sugeridas</div>
 
 
-          <div class="w-full h-128 bg-white rounded-lg p-5 overflow-y-auto">
+          <div class="w-full h-128 bg-white rounded-lg p-5 overflow-y-auto border border-gray-300">
             <ul>
 
               <li v-for="route in filteredRoutes" :key="route" class="hover:text-primary">
